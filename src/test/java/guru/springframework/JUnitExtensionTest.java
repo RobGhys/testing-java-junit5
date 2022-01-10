9807c4560b19b839501f7a16097f3eb864ab52e9
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-10 07:45:34 +0100

    Mock test on delete method

--- src/test/java/guru/springframework/JUnitExtensionTest.java
+++ src/test/java/guru/springframework/JUnitExtensionTest.java
@@ -1,21 +1,19 @@
-package guru.springframework.sfgpetclinic;
+package guru.springframework;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Map;
 
-public class AnnotationMocksTest {
+@ExtendWith(MockitoExtension.class)
+public class JUnitExtensionTest {
 
     @Mock
     Map<String, Object> mapMock;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     void testMock() {
