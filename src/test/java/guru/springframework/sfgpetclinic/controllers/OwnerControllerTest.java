4f982de34c5cc717256aa710886928a7fab89579
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-10 11:23:28 +0100

    Added a beforeEach in OwnerControllerTest

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,14 +1,13 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -37,33 +36,66 @@ class OwnerControllerTest {
     @Captor
     ArgumentCaptor<String> stringArgumentCaptor;
 
-    @Test
-    void processFindFormWildcardStringTest() {
-        // Given
-        Owner owner = new Owner(1l, "Jean", "Dupont");
-        List<Owner> owners = new ArrayList<>();
-        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(owners);
+    @BeforeEach
+    void setUp() {
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture()))
+                .willAnswer(invocation -> {
+                    List<Owner> owners = new ArrayList<>();
+                    String name = invocation.getArgument(0);
 
-        // When
-        String viewName = ownerController.processFindForm(owner, bindingResult, null);
+                    if (name.equals("%Dupont%")) {
+                       owners.add(new Owner(1l, "Jean", "Dupont"));
+                       return owners;
+                    } else if (name.equals("%DontFindMe%")) {
+                        // Empty list
+                        return owners;
+                    } else if (name.equals("%FindMe%")) {
+                        owners.add(new Owner(1l, "Jean", "Dupont"));
+                        owners.add(new Owner(2l, "Jean2", "Dupont2"));
+                        return owners;
+                    }
 
-        // Then
-        assertThat("%Dupont%").isEqualToIgnoringCase(captor.getValue());
+                    throw new RuntimeException("Invalid Argument");
+        });
     }
 
     @Test
     void processFindFormWildcardStringAnnotationTest() {
         // Given
         Owner owner = new Owner(1l, "Jean", "Dupont");
-        List<Owner> owners = new ArrayList<>();
-        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(owners);
 
         // When
         String viewName = ownerController.processFindForm(owner, bindingResult, null);
 
         // Then
         assertThat("%Dupont%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("redirect:/owners/1").isEqualToIgnoringCase(viewName);
+    }
+
+    @Test
+    void processFindFormWildcardNotFoundTest() {
+        // Given
+        Owner owner = new Owner(2l, "Thomas", "DontFindMe");
+
+        // When
+        String viewName = ownerController.processFindForm(owner, bindingResult, null);
+
+        // Then
+        assertThat("%DontFindMe%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("owners/findOwners").isEqualToIgnoringCase(viewName);
+    }
+
+    @Test
+    void processFindFormWildcardFoundTest() {
+        // Given
+        Owner owner = new Owner(3l, "Thomas", "FindMe");
+
+        // When
+        String viewName = ownerController.processFindForm(owner, bindingResult, Mockito.mock(Model.class));
+
+        // Then
+        assertThat("%FindMe%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("owners/ownersList").isEqualToIgnoringCase(viewName);
     }
 
     @Test
